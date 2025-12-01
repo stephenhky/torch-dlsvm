e3b066c34fbb7022d03df5c7d7181139e906d125
stephenhky <stephenhky@yahoo.com.hk>
2025-12-01 17:33:18 -0500

    added type hinting in dataset

--- test/test_iris.py
+++ test/test_iris.py
@@ -9,10 +9,10 @@ class IrisDataset(Dataset):
     def __init__(self):
         self._iris = load_iris()
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._iris['target'])
 
-    def __getitem__(self, idx: int):
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, int]:
         return torch.Tensor(self._iris['data'][idx, :]), self._iris['target'][idx]
 
 
@@ -21,7 +21,7 @@ class BinaryIrisDataset(IrisDataset):
         super().__init__()
         self._ref_target = ref_target
 
-    def __getitem__(self, idx: int):
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, int]:
         x, iris_target = super().__getitem__(idx)
         if iris_target == self._ref_target:
             return x, 1
