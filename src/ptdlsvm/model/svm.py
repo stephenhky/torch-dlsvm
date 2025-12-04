1f19a2978ac903f054d2375069f537b676d5cbf4
stephenhky <stephenhky@yahoo.com.hk>
2025-12-04 10:48:19 -0500

    usr matmul instead of dot

--- src/ptdlsvm/model/svm.py
+++ src/ptdlsvm/model/svm.py
@@ -12,4 +12,4 @@ class SVM(torch.nn.Module):
         self.b = torch.nn.Parameter(torch.rand(1), requires_grad=True)
 
     def forward(self, x: torch.Tensor):
-        return torch.dot(x, self.w.T) - self.b
+        return torch.matmul(x, self.w.T) - self.b
