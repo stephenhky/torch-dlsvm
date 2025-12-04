2c46be93b524b56197d7b20605c08b5007ac3d49
stephenhky <stephenhky@yahoo.com.hk>
2025-12-04 10:17:38 -0500

    SVM module

--- src/ptdlsvm/model/svm.py
+++ src/ptdlsvm/model/svm.py
@@ -6,6 +6,7 @@ import torch
 
 class SVM(torch.nn.Module):
     def __init__(self, nbfeatures: int):
+        super().__init__()
         self.nbfeatures = nbfeatures
         self.w = torch.nn.Parameter(torch.rand(self.nbfeatures), requires_grad=True)
         self.b = torch.nn.Parameter(torch.rand(1), requires_grad=True)
