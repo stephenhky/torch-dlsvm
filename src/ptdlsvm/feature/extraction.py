8b6c0c138fd233b4c3dfbecf3d74f546c5e70e7d
stephenhky <stephenhky@yahoo.com.hk>
2025-12-09 15:16:12 -0500

    updated feature extraction

--- src/ptdlsvm/feature/extraction.py
+++ src/ptdlsvm/feature/extraction.py
@@ -2,6 +2,7 @@
 from typing import Optional, Self
 import warnings
 from os import PathLike
+from collections import defaultdict
 
 import torch
 import orjson
@@ -27,16 +28,17 @@ class BOWFeatureExtractor(FeatureExtractor):
         if isinstance(X, str):
             X = [X]
 
-        feature_pos = [
-            self._features[feature] for feature in X if feature in self._features
-        ]
+        feature_pos_count = defaultdict(lambda : 0)
+        for feature in X:
+            if feature in self._features:
+                feature_pos_count[self._features[feature]] += 1
 
         return torch.sparse_coo_tensor(
             torch.stack((
-                torch.zeros(len(feature_pos), dtype=torch.int64),
-                torch.IntTensor(feature_pos)
+                torch.zeros(len(feature_pos_count), dtype=torch.int64),
+                torch.IntTensor(list(feature_pos_count.keys()))
             ), dim=0),
-            torch.ones(len(feature_pos)),
+            torch.FloatTensor(list(feature_pos_count.values())),
             (1, len(self._features))
         )
 
